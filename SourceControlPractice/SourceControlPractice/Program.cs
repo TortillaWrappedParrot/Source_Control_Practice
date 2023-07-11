d4ff3632d9b5d3b34a76b31537facfe2a98b058b
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-07-11 09:17:40 -0700

    Added switch for input

--- SourceControlPractice/SourceControlPractice/Program.cs
+++ SourceControlPractice/SourceControlPractice/Program.cs
@@ -10,6 +10,28 @@ namespace SourceControlPractice
     {
         static void Main(string[] args)
         {
+            int sentinel = 0;
+            int assignment = 0;
+            while (sentinel != 5) {
+                bool result = int.TryParse(Console.ReadLine(),out _);
+
+                switch (sentinel)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
         }
     }
 }
