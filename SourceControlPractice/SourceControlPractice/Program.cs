680fd751c03c16c3ceecd1292c6a379f91cea9b7
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-07-11 09:37:16 -0700

    Finished basic calculator

--- SourceControlPractice/SourceControlPractice/Program.cs
+++ SourceControlPractice/SourceControlPractice/Program.cs
@@ -19,16 +19,59 @@ namespace SourceControlPractice
                 Console.WriteLine("3 - Whitespace Remover");
                 Console.WriteLine("4 - Guessing Game");
                 Console.WriteLine("5 - Exit");
-                bool result = int.TryParse(Console.ReadLine(),out _);
-                while (result == false)
+                Console.Write("Enter your number: ");
+                string line = Console.ReadLine();
+                bool result = int.TryParse(line,out _);
+                while (result == false && result != true)
                 {
                     Console.WriteLine("Please enter a valid number");
                     result = int.TryParse(Console.ReadLine(), out _);
                 }
-
+                assignment = int.Parse(line);
                 switch (assignment)
                 {
                     case 1:
+                        Console.WriteLine("Enter the first int");
+                        string num1Input = Console.ReadLine();
+                        bool num1Check = int.TryParse(num1Input, out _);
+                        while (num1Check == false)
+                        {
+                            Console.WriteLine("Please enter a valid int");
+                            num1Check = int.TryParse(Console.ReadLine(), out _);
+                        }
+                        Console.WriteLine("Enter your comparetor");
+                        string compareterInput = Console.ReadLine().Trim();
+                        while (compareterInput != "/" && compareterInput != "*" && compareterInput != "+" && compareterInput != "-")
+                        {
+                            Console.WriteLine("Please enter a valid comparter");
+                            compareterInput = Console.ReadLine().Trim();
+                        }
+                        Console.WriteLine("Enter the second int");
+                        string num2Input = Console.ReadLine();
+                        bool num2Check = int.TryParse(num2Input, out _);
+                        while (num2Check == false)
+                        {
+                            Console.WriteLine("Please enter a valid int");
+                            num2Check = int.TryParse(Console.ReadLine(), out _);
+                        }
+                        int num1 = int.Parse(num1Input);
+                        int num2 = int.Parse(num2Input);
+                        switch (compareterInput)
+                        {
+                            case "*":
+                                Console.WriteLine(num1 * num2);
+                                break;
+                            case "+":
+                                Console.WriteLine(num1 + num2);
+                                break;
+                            case "-":
+                                Console.WriteLine(num1 - num2);
+                                break;
+                            case "/":
+                                Console.WriteLine(num1 / num2);
+                                break;
+                        }
+                        Console.WriteLine("-------------------------------");
                         break;
                     case 2:
                         break;
