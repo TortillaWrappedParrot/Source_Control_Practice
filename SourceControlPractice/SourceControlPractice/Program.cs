13c9ef8d2583ec0ca362d5eefe6bf0c16e03faa1
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-07-11 09:20:33 -0700

    Created user instructions

--- SourceControlPractice/SourceControlPractice/Program.cs
+++ SourceControlPractice/SourceControlPractice/Program.cs
@@ -13,9 +13,20 @@ namespace SourceControlPractice
             int sentinel = 0;
             int assignment = 0;
             while (sentinel != 5) {
+                Console.WriteLine("Please enter the number for the program you need");
+                Console.WriteLine("1 - Basic Calculator");
+                Console.WriteLine("2 - Line Trimmer");
+                Console.WriteLine("3 - Whitespace Remover");
+                Console.WriteLine("4 - Guessing Game");
+                Console.WriteLine("5 - Exit");
                 bool result = int.TryParse(Console.ReadLine(),out _);
+                while (result == false)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    result = int.TryParse(Console.ReadLine(), out _);
+                }
 
-                switch (sentinel)
+                switch (assignment)
                 {
                     case 1:
                         break;
